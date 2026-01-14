1e2da72c0e18fac3dc0cd83609e0b9bab6c6a685
Raian-stack <lorai014789@gmail.com>
2026-01-14 19:46:18 +0800

    changed the to ryan and added a text "handsome"

--- app/src/main/java/com/example/smartcompanionapp/MainActivity.kt
+++ app/src/main/java/com/example/smartcompanionapp/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             SmartCompanionAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Ryan",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Hello handsome $name!",
         modifier = modifier
     )
 }
