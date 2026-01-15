9fa4ed69bbe4b6930bf3fa7cccecb879f56fe8af
K4kei <ianlimjoco28@gmail.com>
2026-01-15 20:38:41 +0800

    Added Task Screen, Data Models for tasks, added navigation

--- app/src/main/java/com/example/smartcompanionapp/MainActivity.kt
+++ app/src/main/java/com/example/smartcompanionapp/MainActivity.kt
@@ -3,45 +3,17 @@ package com.example.smartcompanionapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.smartcompanionapp.ui.theme.SmartCompanionAppTheme
+import com.example.smartcompanionapp.ui.navigation.AppNavigation
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             SmartCompanionAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Ryan",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                AppNavigation()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello handsome $name!, test test test",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SmartCompanionAppTheme {
-        Greeting("Android")
     }
 }
